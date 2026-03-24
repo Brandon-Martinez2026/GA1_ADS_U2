1a643cc0ad82a6265a871e60a7895ee88ec7fa66
brandon_martinez <brandonmartinez@emilianisomascos.edu.gt>
2026-03-24 10:46:27 -0600

    Ejercicio 7-8 y word

---
+++ E7_BWMC.py
@@ -0,0 +1,5 @@
+#BWMC
+
+Luz_encendida = False
+
+print("¿La luz está encendida en el laboratorio #5 de Computacion?", Luz_encendida)
---
+++ E8_BWMC.py
@@ -0,0 +1,8 @@
+#BWMC
+
+nombre = "Brandon Waldemar"
+apellido = "Martinez Cano "
+
+nombre_completo = nombre + " " + apellido
+
+print("Nombre completo: ",nombre_completo)
